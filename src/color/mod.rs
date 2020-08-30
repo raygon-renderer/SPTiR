fed368fcd1a10958015d5dd26d84e53ef761d321
novacrazy <novacrazy@gmail.com>
2020-08-30 15:39:49 -0500

    Add some docs for color spaces

--- src/color/mod.rs
+++ src/color/mod.rs
@@ -8,35 +8,52 @@ pub mod spaces;
 
 pub use self::matrix::ColorMatrix3;
 
+/// A two-dimensional coordinate on the xy-chromaticity diagram, with an associated luminance.
 #[allow(non_camel_case_types)]
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct xyY {
     pub x: f32,
     pub y: f32,
-    pub y_prime: f32,
+    pub luminance: f32,
 }
 
 impl xyY {
-    pub const fn new(x: f32, y: f32, y_prime: f32) -> xyY {
-        xyY { x, y, y_prime }
+    pub const fn new(x: f32, y: f32, luminance: f32) -> xyY {
+        xyY { x, y, luminance }
     }
 
+    /**
+        Converts xyY to XYZ tristimulus values
+
+        ```math
+        \begin{aligned}
+            X &= \frac{x Y}{y} \\
+            Y &= Y \\
+            Z &= \frac{(1 - x - y) Y}{y}
+        \end{aligned}
+        ```
+    */
     pub const fn to_xyz(self) -> XYZSpectrum {
-        let xyY { x, y, y_prime } = self;
+        let xyY { x, y, luminance } = self;
 
-        let wx = y_prime * x / y;
-        let wy = y_prime;
-        let wz = y_prime * (1.0 - x - y) / y;
+        let wx = luminance * x / y;
+        let wy = luminance;
+        let wz = luminance * (1.0 - x - y) / y;
 
-        XYZSpectrum::new(wx as f32, wy as f32, wz as f32)
+        XYZSpectrum::new(wx, wy, wz)
     }
 }
 
+/// Defines a working space within the human chromaticity diagram for which color operations can occur
 pub trait Colorspace {
+    /// Defines the coordinates in the xy-chromaticity diagram for which each RGB primary is placed
     const PRIMARIES: RGBPrimaries;
+    /// Defines the standard illuminant for the color space
     const WHITEPOINT: xyY;
 
+    /// Conversion matrix from Linear RGB to XYZ tristimulus values
     const RGB_TO_XYZ: ColorMatrix3 = Self::PRIMARIES.rgb_to_xyz(Self::WHITEPOINT);
+    /// Conversion matrix from XYZ tristimulus values to Linear RGB
     const XYZ_TO_RGB: ColorMatrix3 = Self::RGB_TO_XYZ.inverse();
 }
 
@@ -57,8 +74,24 @@ pub mod whitepoints {
     pub const ACES: xyY = xyY::new(0.32168, 0.33767, 1.0);
 }
 
+/// Helper struct for Chromatic Adaptation, converting from one white-point to another.
+///
+/// Since color spaces are defined not only by their RGB Primaries on the xy-chromaticity chart, but
+/// also their white-point, it is non-trivial to convert RGB to RGB between color spaces.
+///
+/// The implementation provided here uses the Bradford method of Chromatic Adaptation,
+/// which has been shown to be the most perceptually accurate.
 pub struct Adaptation<FROM: Colorspace, TO: Colorspace>(PhantomData<(FROM, TO)>);
 
+/**
+    ```math
+    \begin{bmatrix}
+        +0.89510  & +0.26640 & -0.16140 \\
+        -0.75020  & +1.71350 & +0.03670 \\
+        +0.03890  & -0.06850 & +1.02960 \\
+    \end{bmatrix}
+    ```
+*/
 #[rustfmt::skip]
 pub const FORWARD_BRADFORD_MATRIX: ColorMatrix3 = ColorMatrix3([
     0.89510,  0.26640, -0.16140,
@@ -66,12 +99,37 @@ pub const FORWARD_BRADFORD_MATRIX: ColorMatrix3 = ColorMatrix3([
     0.03890, -0.06850,  1.02960,
 ]);
 
+/// Inverse of [`FORWARD_BRADFORD_MATRIX`](./constant.FORWARD_BRADFORD_MATRIX.html), computed automatically
 pub const INVERSE_BRADFORD_MATRIX: ColorMatrix3 = FORWARD_BRADFORD_MATRIX.inverse();
 
+/**
+    Computes a linear transform to adapt one whitepoint to another, using the Bradford response domains.
+
+    ```math
+    \begin{aligned}
+    \begin{bmatrix} X_{D} \\ Y_{D} \\ Z_{D} \end{bmatrix} &=
+    \begin{bmatrix} M \end{bmatrix}
+    \begin{bmatrix} X_{S} \\ Y_{S} \\ Z_{S} \end{bmatrix} \\
+    \text{where:} \\ \;
+    \begin{bmatrix} M \end{bmatrix} &= \begin{bmatrix} M_{A} \end{bmatrix}^{-1}
+    \begin{bmatrix}
+        \rho_{D}/\rho_{S} & 0 & 0 \\
+        0 & \gamma_{D}/\gamma_{S} & 0 \\
+        0 & 0 & \beta_{D} / \beta_{S}
+    \end{bmatrix}
+    \begin{bmatrix} M_{A} \end{bmatrix} \\
+    \text{where:} \\ \;
+    \begin{bmatrix} \rho_{S} \\ \gamma_{S} \\ \beta_{S} \end{bmatrix} &=
+        \begin{bmatrix} M_{A} \end{bmatrix} \begin{bmatrix} X_{WS} \\ Y_{WS} \\ Z_{WS} \end{bmatrix} \\ \;
+    \begin{bmatrix} \rho_{D} \\ \gamma_{D} \\ \beta_{D} \end{bmatrix} &=
+        \begin{bmatrix} M_{A} \end{bmatrix} \begin{bmatrix} X_{WD} \\ Y_{WD} \\ Z_{WD} \end{bmatrix}
+    \end{aligned}
+    ```
+*/
 #[rustfmt::skip]
 pub const fn compute_adaptation_matrix(swp: xyY, dwp: xyY) -> ColorMatrix3 {
     // no floating point error if same
-    if swp.x == dwp.x && swp.y == dwp.y && swp.y_prime == dwp.y_prime {
+    if swp.x == dwp.x && swp.y == dwp.y && swp.luminance == dwp.luminance {
         return ColorMatrix3::IDENTITY;
     }
 
@@ -92,6 +150,7 @@ impl<FROM: Colorspace, TO: Colorspace> Adaptation<FROM, TO> {
     pub const RGB_TO_RGB: ColorMatrix3 = TO::XYZ_TO_RGB.multiply(Self::XYZ_TO_XYZ).multiply(FROM::RGB_TO_XYZ);
 }
 
+/// Color consisting of Red, Green and Blue components, defined within a specific colorspace.
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct RGBColor<CS: Colorspace> {
     pub r: f32,
@@ -112,6 +171,7 @@ impl<CS: Colorspace> RGBColor<CS> {
     }
 }
 
+/// Defines the coordinates in the xy-chromaticity diagram for which each RGB primary is placed
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct RGBPrimaries {
     pub r: xyY,
@@ -120,6 +180,41 @@ pub struct RGBPrimaries {
 }
 
 impl RGBPrimaries {
+    /**
+    Derives the linear transformation matrix `$\begin{bmatrix}M\end{bmatrix}$` between RGB and XYZ tristimulus values,
+    using the xyY RGB primaries and the whitepoint (here denoted by `$\tiny \begin{bmatrix} X_W \\ Y_W \\ Z_W \end{bmatrix}$`)
+
+    ```math
+    \begin{aligned}
+        \begin{bmatrix} X \\ Y \\ Z \end{bmatrix} &=
+        \begin{bmatrix} M \end{bmatrix}
+        \begin{bmatrix} R \\ G \\ B \end{bmatrix}
+
+        \\ \text{where:} \\ \;
+
+        M &= \begin{bmatrix}
+            S_r X_r & S_g X_g & S_b X_b \\
+            S_r Y_r & S_g Y_g & S_b Y_b \\
+            S_r Z_r & S_g Z_g & S_b Z_b
+        \end{bmatrix}
+        \\ \text{where:} \\ \;
+        \begin{bmatrix} S_r \\ S_g \\ S_b \end{bmatrix} &=
+        P_{xyz}^{-1}
+        \begin{bmatrix} X_W \\ Y_W \\ Z_W \end{bmatrix}
+        \\
+        P_{xyz} &= \begin{pmatrix}
+            X_r & Y_r & Z_r \\
+            X_g & Y_g & Z_g \\
+            X_b & Y_b & Z_b
+        \end{pmatrix} =
+        \begin{pmatrix}
+            \frac{x_r Y_r}{y_r} & Y_r & \frac{(1 - x_r - y_r) Y_r}{y_r} \\ \;
+            \frac{x_g Y_g}{y_g} & Y_g & \frac{(1 - x_g - y_g) Y_g}{y_g} \\ \;
+            \frac{x_b Y_b}{y_b} & Y_b & \frac{(1 - x_b - y_b) Y_b}{y_b}
+        \end{pmatrix}
+    \end{aligned}
+    ```
+    */
     #[rustfmt::skip]
     pub const fn rgb_to_xyz(self, wp: xyY) -> ColorMatrix3 {
         let RGBPrimaries { r, g, b } = self;
--- src/color/spaces.rs
+++ src/color/spaces.rs
@@ -4,11 +4,14 @@ pub type SRGBColor = RGBColor<SRGB>;
 pub type ACES2065_1Color = RGBColor<ACES_AP0>;
 pub type ACEScgColor = RGBColor<ACES_AP1>;
 
+/// ACES 2065-1 Extreme Wide-Gamut Color Space
 #[allow(non_camel_case_types)]
 pub struct ACES_AP0;
+/// ACEScg Wide-Gamut Color Space
 #[allow(non_camel_case_types)]
 pub struct ACES_AP1;
 
+/// Rec701 sRGB Color Space
 pub struct SRGB;
 
 impl Colorspace for ACES_AP0 {
@@ -42,6 +45,21 @@ impl Colorspace for SRGB {
 }
 
 impl SRGBColor {
+    /**
+    Decodes an 8-bit encoded sRGB color.
+
+    ```math
+    \gamma ^{-1}(u)={
+        \begin{cases}{
+            \frac {u}{12.92}} &= {\frac {25u}{323}}&u\leq 0.04045 \\
+            \left({\tfrac {u+0.055}{1.055}}\right)^{2.4} &= \left({\tfrac {200u+11}{211}}\right)^{\frac {12}{5}}&{\text{otherwise}}
+        \end{cases}
+    }
+    ```
+    * where `$u$` is `$R_{srgb}, G_{srgb}, \text{or}\ B_{srgb}$`
+
+    **NOTE**: Input slice must have a length of at least 3.
+    */
     pub fn decode_transfer_function_from(srgb: &[u8]) -> SRGBColor {
         assert!(srgb.len() >= 3);
 
@@ -60,6 +78,21 @@ impl SRGBColor {
         )
     }
 
+    /**
+    Encodes a linear sRGB color to an output slice of 8-bit integers.
+
+    ```math
+    \gamma (u)={
+        \begin{cases}
+            12.92u &= {\frac {323u}{25}}&u\leq 0.0031308 \\
+            1.055u^{1/2.4}-0.055 &= {\frac {211u^{\frac {5}{12}}-11}{200}}&{\text{otherwise}}
+        \end{cases}
+    }
+    ```
+    * where `$u$` is `$R_{Linear}, G_{Linear}, \text{or}\ B_{Linear}$`
+
+    **NOTE**: Output slice must have a length of at least 3.
+    */
     pub fn encode_transfer_function_to(&self, srgb: &mut [u8]) {
         assert!(srgb.len() >= 3);
 
