6c7e53753b673949af28352393e0e2c6553fa6aa
novacrazy <novacrazy@users.noreply.github.com>
2020-08-31 13:02:47 +0000

    deploy: 9afd0bce8052893e2e70ac86d93462743ffde21b

---
+++ implementors/core/ops/arith/trait.Add.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["sptir"] = [{"text":"impl Add&lt;XYZSpectrum&gt; for XYZSpectrum","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
