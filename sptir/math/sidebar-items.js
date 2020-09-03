677618f48fd304b63cd2302c1ea94b6e36be48f4
novacrazy <novacrazy@users.noreply.github.com>
2020-09-03 00:08:57 +0000

    deploy: a7fabd5b13c2fcb065dd52486ff818d09859c5ff

--- sptir/math/sidebar-items.js
+++ sptir/math/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["integrate",""]]});
+initSidebarItems({"fn":[["integrate","Computes `$\\int_a^b f(x) dx$` with an adaptive Simpson's method, to the given precision and recursion depth."]]});
